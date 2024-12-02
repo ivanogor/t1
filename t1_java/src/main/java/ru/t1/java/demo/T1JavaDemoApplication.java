aad0efd9e553110a1d21c5d2f8fea1f9713dd883
Ivan Ogor <ivanogor23@gmail.com>
2024-12-02 10:30:08 +0800

    Added annotation

--- t1_java/src/main/java/ru/t1/java/demo/T1JavaDemoApplication.java
+++ t1_java/src/main/java/ru/t1/java/demo/T1JavaDemoApplication.java
@@ -3,9 +3,11 @@ package ru.t1.java.demo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @Slf4j
+@EnableScheduling
 public class T1JavaDemoApplication {
 
     public static void main(String[] args) {
